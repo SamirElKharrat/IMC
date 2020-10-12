9e7fc76f17fb9d9330576d46e93b2db36ed60a10
SamirElKharrat <samirelkharrat@gmail.com>
2020-10-12 14:25:52 +0100

    Añadimos los binding y comprobamos el funcionamiento del programa

--- src/main/java/dad/imc/IMC.java
+++ src/main/java/dad/imc/IMC.java
@@ -1,25 +1,27 @@
 package dad.imc;
 
-import java.time.LocalDate;
-
 import javafx.application.Application;
+import javafx.beans.binding.Binding;
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.event.ActionEvent;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.converter.NumberStringConverter;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 
 public class IMC extends Application {
 
@@ -27,20 +29,26 @@ public class IMC extends Application {
 	private Label masacorporal, indice;
 
 	private DoubleProperty peso = new SimpleDoubleProperty(0);
+	private StringProperty pesotxtProperty = new SimpleStringProperty();
 	private DoubleProperty altura = new SimpleDoubleProperty(0);
+	private StringProperty alturatxtProperty = new SimpleStringProperty();
 	private DoubleProperty IMC = new SimpleDoubleProperty(0);
+	private StringProperty imcProperty = new SimpleStringProperty();
 
 	public void IndicadorMasaCorporal() {
 
-		if (IMC.get() < 0) {
+		if (IMC.get() <= 0) {
+			indice.setText("(peso * altura^ 2)");
+			masacorporal.setText("Bajo Peso | Normal | Sobrepeso | Obeso");
+			
 
 		} else {
 			indice.setText("IMC: " + IMC.get());
-			if(IMC.get() < 18.5)
+			if (IMC.get() < 18.5)
 				masacorporal.setText("Bajo Peso");
-			else if(IMC.get() >= 18.5 && IMC.get() < 25)
+			else if (IMC.get() >= 18.5 && IMC.get() < 25)
 				masacorporal.setText("Normal");
-			else if(IMC.get() >= 25 && IMC.get() < 30)
+			else if (IMC.get() >= 25 && IMC.get() < 30)
 				masacorporal.setText("Sobrepeso");
 			else
 				masacorporal.setText("Obeso");
@@ -52,8 +60,8 @@ public class IMC extends Application {
 		if ((peso.get() == 0d) || (altura.get() == 0d))
 			IMC.set(0d);
 		else {
-			Double resultado = (peso.get() / (altura.get() * altura.get())) * 10000d;
-			IMC.set(Math.round(resultado * 100.0));
+			Double resultado = (peso.get() / (altura.get() * altura.get()) * 1000d);
+			IMC.set(Math.round(resultado * 10.0));
 		}
 	}
 
@@ -82,11 +90,23 @@ public class IMC extends Application {
 
 		masacorporal = new Label("Bajo Peso | Normal | Sobrepeso | Obeso");
 
+		pesotxtProperty.bindBidirectional(pesotxt.textProperty());
+		Bindings.bindBidirectional(pesotxtProperty, peso, new NumberStringConverter());
+		peso.addListener((o, ov, nv) -> calculoIMC());
+
+		alturatxtProperty.bindBidirectional(alturatxt.textProperty());
+		Bindings.bindBidirectional(alturatxtProperty, altura, new NumberStringConverter());
+		altura.addListener((o, ov, nv) -> calculoIMC());
+
+		imcProperty.bindBidirectional(textoindice.textProperty());
+		Bindings.bindBidirectional(imcProperty, IMC, new NumberStringConverter());
+		IMC.addListener((o, ov, nv) -> IndicadorMasaCorporal());
+
 		// creamos un panel con disposición vertical
 		VBox root = new VBox();
 		root.setSpacing(5);
 		root.setAlignment(Pos.CENTER);
-		root.getChildren().addAll(pesoBox, alturaBox, indiceBox, masacorporal);
+		root.getChildren().addAll(pesoBox, alturaBox, indiceBox, indice, masacorporal);
 
 		// creamos la escena
 		Scene escena = new Scene(root, 320, 200);
